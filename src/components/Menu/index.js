b49b01c566a35c183a6776b8236945415143586f
gibifyOfficial <evandrogibicoski@outlook.com>
2020-07-28 16:26:08 -0300

    add route, css, e refactoring the folders from project

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Logo from '../../assets/Logo.svg';
 import './styles.css'
 import Button from '../Button';
@@ -6,11 +8,11 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">=
-            <a href="/">
+            <Link to="/">
                <img className="Logo" src={Logo} alt="logo"/>
-            </a>
+            </Link>
 
-            <Button className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro-videos">
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import  CadastroVideos  from './pages/CadastroVideos';
+import CadastroCategoria from './pages/CadastroCategoria';
+import Home from './pages/Home';
 import './index.css';
-import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro-videos" component={CadastroVideos} />
+      <Route path="/cadastro-categoria" component={CadastroCategoria} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
